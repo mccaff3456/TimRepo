56432df2ccc764abfb2480153cac78d922ea2fbc
Tim Mc <mccaff3456@gmail.com>
2023-07-25 14:21:59 -0400

    update Stocks and Tim_Resume

--- Stock_BuySell_App/script.js
+++ Stock_BuySell_App/script.js
@@ -35,4 +35,58 @@ function generateSignals(stockSymbol) {
       resultDiv.appendChild(signalDiv);
     });
   });
+  async function fetchHistoricalPrices(symbol, days) {
+    const apiKey = 'YOUR_API_KEY'; // Replace with your API key
+    const apiUrl = `https://api.example.com/stock-prices?symbol=${symbol}&days=${days}&apiKey=${apiKey}`;
+  
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw new Error('Failed to fetch historical prices.');
+    }
+  }
+  /* AI generated Code To calculate the minimum stock price for symbol TNA
+   over the last 20 days, you would need access to a data source that provides 
+   historical stock prices for the symbol TNA. 
+   Here's an example of how you could use a function to fetch the 
+   historical stock prices and calculate the minimum price:  */
+   
+   calculateMinStockPrice('TNA', 20);
+  function calculateMinStockPrice(symbol, days) {
+    fetchHistoricalPrices(symbol, days)
+      .then(data => {
+        const prices = data.prices;
+        if (prices.length < 1) {
+          throw new Error('No historical prices available for the symbol.');
+        }
+  
+        let minPrice = prices[0].price;
+  
+        for (let i = 1; i < prices.length; i++) {
+          const price = prices[i].price;
+          if (price < minPrice) {
+            minPrice = price;
+          }
+        }
+  
+        console.log(`Minimum stock price for ${symbol} over the last ${days} days: ${minPrice}`);
+      })
+      .catch(error => {
+        console.error(error.message);
+      });
+  }
+  // Example of Math functions.
+  function calculateY() {
+    var maxVal = Math.max(5, 7, 6);
+    var minVal = Math.min(3, 2, 4);
+    var y = maxVal / minVal;
+    return y;
+  }
+    function calculateY(x) {
+      var y = (Math.pow(x, 2) + (3 * x) + 12) / 4;
+      return y;
+    }
+  
   
